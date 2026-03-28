a4cd28519bbfdd50bf4d6c0111fa76aa0f0ac7a6
yarden <yarden.levy@abra-it.com>
2026-03-28 22:42:33 +0300

    feat: offline support

--- src/app/manga/page.tsx
+++ src/app/manga/page.tsx
@@ -1,7 +1,7 @@
-'use client';
-
 import { Suspense } from 'react';
 import { MangaContent } from './components/MangaContent';
+
+export const dynamic = 'force-static';
 
 const MangaDetailPage = () => {
   return (
